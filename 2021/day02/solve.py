a0889318d1ee90abea224fae508aa3b77a4a038c
paul <paul.hildebrandt@tum.de>
2021-12-02 16:03:30 +0100

    day02

---
+++ 2021/day02/solve.py
@@ -0,0 +1,32 @@
+with open('input.txt', 'r') as f:
+    commands = f.read().splitlines()
+
+def part1():
+    depth,horizontal = 0,0
+    for x in range(len(commands)):
+        direction = commands[x].split()[0]
+        value = int(commands[x].split()[1])
+        if direction == 'forward':
+            horizontal += value
+        if direction == 'down':
+            depth += value
+        if direction == 'up':
+            depth -= value
+    return depth * horizontal
+
+def part2():
+    depth,horizontal,aim = 0,0,0
+    for x in range(len(commands)):
+        direction = commands[x].split()[0]
+        value = int(commands[x].split()[1])
+        if direction == 'forward':
+            horizontal += value
+            depth += aim * value
+        if direction == 'down':
+            aim += value
+        if direction == 'up':
+            aim -= value
+    return depth * horizontal
+
+print(f"Part 1: {part1()}")
+print(f"Part 2: {part2()}")
