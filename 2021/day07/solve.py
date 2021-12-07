d4da15a1cc50c20781d4cf34d04b472ff325a297
paul <paul.hildebrandt@tum.de>
2021-12-07 12:17:10 +0100

    day07

---
+++ 2021/day07/solve.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+pos = np.loadtxt('input.txt',delimiter=',',dtype=int)
+
+def part1():
+    return np.abs(pos-int(np.median(pos))).sum()
+
+def part2():
+    return int(((np.abs(pos-(round(np.mean(pos))-1))/2)*(np.abs(pos-(round(np.mean(pos))-1))+1)).sum())
+
+print(f"Part 1: {part1()}")
+print(f"Part 2: {part2()}")
