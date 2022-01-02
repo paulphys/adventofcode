4e8a0da56e92640348865ba443119519c7b9e3d0
paul <paul.hildebrandt@tum.de>
2022-01-02 13:08:18 +0100

    day01

--- 2021/day01/solve.py
+++ 2021/day01/solve.py
@@ -1,4 +1,4 @@
-input = open("input.txt").read()
+input = [int(x) for x in open("input.txt").read().splitlines()]
 
 def count(input):
     increments = 0
@@ -9,8 +9,7 @@ def count(input):
         previous = i
     return increments
 
-measurements = [int(x) for x in input.splitlines()]
-summed_windows = [sum(x) for x in zip(measurements, measurements[1:], measurements[2:])]
+summed_windows = [sum(x) for x in zip(input, input[1:], input[2:])]
 
-print(f"Part 1: {count(measurements)}")
+print(f"Part 1: {count(input)}")
 print(f"Part 2: {count(summed_windows)}")
