bd46e9371f046196b2b7a117ddb1f56fa0b9aab6
paulphys <paul.hildebrandt@tum.de>
2022-12-02 13:35:56 +0100

    day02

--- 2022/day02/solve.py
+++ 2022/day02/solve.py
@@ -1,5 +1,3 @@
-# dictionary time
-
 guide = [game.strip() for game in open("input.txt").readlines()]
 p1 = {"A X": 4, "A Y": 8, "A Z": 3, "B X": 1, "B Y": 5, "B Z": 9, "C X": 7, "C Y": 2, "C Z": 6}
 p2 = {"A X": 3, "A Y": 4, "A Z": 8, "B X": 1, "B Y": 5, "B Z": 9, "C X": 2, "C Y": 6, "C Z": 7}
