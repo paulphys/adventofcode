29268b49ce8ea2a98bc29abbe46723cb363441b9
paulphys <paul.hildebrandt@tum.de>
2022-12-02 13:34:48 +0100

    day02

--- 2022/day01/solve.py
+++ 2022/day01/solve.py
@@ -1,4 +1,4 @@
 calories = open("input.txt").read().split("\n\n")
-data = sorted(sum(int(snack) for snack in line.splitlines()) for line in calories)
-print(f"p1: {data[-1]}")
-print(f"p2: {sum(data[-3:])}")
+most = sorted(sum(int(snack) for snack in line.splitlines()) for line in calories)
+print(f"p1: {most[-1]}")
+print(f"p2: {sum(most[-3:])}")
---
+++ 2022/day02/solve.py
@@ -0,0 +1,9 @@
+# dictionary time
+
+guide = [game.strip() for game in open("input.txt").readlines()]
+p1 = {"A X": 4, "A Y": 8, "A Z": 3, "B X": 1, "B Y": 5, "B Z": 9, "C X": 7, "C Y": 2, "C Z": 6}
+p2 = {"A X": 3, "A Y": 4, "A Z": 8, "B X": 1, "B Y": 5, "B Z": 9, "C X": 2, "C Y": 6, "C Z": 7}
+total_p1 = sum(p1[game] for game in guide)
+total_p2 = sum(p2[game] for game in guide)
+print(f"p1: {total_p1}")
+print(f"p2: {total_p2}")
