51498f33d96ec83a9a153dbb20eda96a676c9ec3
paulphys <paul.hildebrandt@tum.de>
2022-12-04 16:49:27 +0100

    day04

--- 2022/day04/solve.py
+++ 2022/day04/solve.py
@@ -1,20 +1,14 @@
-jobs, overlap = [job.strip() for job in open("input.txt").readlines()], 0
+jobs = [job.strip() for job in open("input.txt").readlines()]
+p1,p2 = 0,0
 
 for pair in jobs:
     p = pair.split(",")
     j1, j2 = [int(x) for x in p[0].split("-")], [int(x) for x in p[1].split("-")]
     if (int(j1[0]) <= int(j2[0]) and int(j1[1]) >= int(j2[1])) or (int(j2[0]) <= int(j1[0]) and int(j2[1]) >= int(j1[1])):
-        overlap += 1
-
-print(f"p1: {overlap}")
-
-overlap = 0
-
-for pair in jobs:
-    p = pair.split(",")
-    j1, j2 = [int(x) for x in p[0].split("-")], [int(x) for x in p[1].split("-")]
+        p1 += 1
     if not (j1[1] < j2[0] or j2[1] < j1[0]):
-        overlap += 1
+        p2 += 1
 
-print(f"p2: {overlap}")
+print(f"p1: {p1}")
+print(f"p2: {p2}")
 
